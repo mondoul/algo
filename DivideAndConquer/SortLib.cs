bfbd5d4b7a686022c7e840becffb833c6d5029b2
arnaud.mondoulet <arnaud.mondoulet@gmail.com>
2014-05-14 16:08:20 +0200

    Initial commit

---
+++ DivideAndConquer/SortLib.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace DivideAndConquer
+{
+    public class SortLib
+    {
+        public long CountInvInFile(string file)
+        {
+            if (!File.Exists(file)) return -1;
+
+            long[] input = File.ReadLines(file).Select(long.Parse).ToArray();
+
+            return CountInv(input);
+        }
+
+        public int CountComparisonInQuickSortFromFile(string file, Func<int[], int, int, int> choosePivot)
+        {
+            if (!File.Exists(file)) return -1;
+
+            int[] input = File.ReadLines(file).Select(int.Parse).ToArray();
+
+            return QuickSort(input, choosePivot);
+        }
+
+        public int[] MergeSort(int[] input)
+        {
+            if (input.Count() <= 1)
+                return input;
+
+            MergeSortRecurs(input);
+            return input;
+        }
+
+        public long CountInv(long[] input)
+        {
+            if (input.Length <= 1)
+                return 0;
+
+            var invCount = MergeSort_CountInvRecurs(input);
+
+            return invCount;
+        }
+
+        public int[] BubbleSort(int[] input)
+        {
+            for (var i = 0; i < input.Length - 1; i++)
+            {
+                for (var j = 0; j < input.Length - i - 1; j++)
+                {
+                    if (input[j + 1] < input[j])
+                    {
+                        var temp = input[j+1];
+                        input[j + 1] = input[j];
+                        input[j] = temp;
+                    }   
+                } 
+            }
+            return input;
+        }
+
+        public int QuickSort(int[] input, Func<int[], int, int, int> choosePivot)
+        {
+            if (input.Length <= 1) return 0;
+
+            var nbComparisons = Partition(input, 0, input.Length-1, choosePivot);
+
+            return nbComparisons;
+        }
+
+        private void ChoosePivot(int[] input, int left, int right, Func<int[], int, int, int> choosePivot)
+        {
+            var pivot = choosePivot(input, left, right);
+            Swap(input, left, pivot); // swap pivot with left-most array value
+        }
+
+        private int Partition(int[] input, int left, int right, Func<int[], int, int, int> choosePivot)
+        {
+            var nbComparisons = right - left;
+            
+            ChoosePivot(input, left, right, choosePivot);
+            var pivot = input[left];
+            var i = left + 1;
+            for (var j = left + 1; j <= right; j++)
+            {
+                if (input[j] >= pivot) continue;
+                if (input[j] != input[i]) {
+                    Swap(input, i, j);
+                }
+                i++; // move separation to the right
+            }
+            Swap(input, left, i - 1); // put pivot back in its place
+
+            if (left < i - 2) // if length of left-most part > 1, recurs
+                nbComparisons += Partition(input, left, i - 2, choosePivot);
+
+            if(i < right) // if length of right-most part > 1, recurs
+                nbComparisons += Partition(input, i, right, choosePivot);
+
+            return nbComparisons;
+        }
+
+        private void Swap(int[] input, int a, int b)
+        {
+            var temp = input[a];
+            input[a] = input[b];
+            input[b] = temp;
+        }
+
+        private void MergeSortRecurs(int[] input)
+        {
+            if (input.Length < 2) return;
+
+            int mid = (input.Length + 1) / 2;
+            int[] left = new int[mid];
+            int[] right = new int[input.Length - mid];
+
+            Array.Copy(input, 0, left, 0, mid);
+            Array.Copy(input, mid, right, 0, input.Length - mid);
+
+            MergeSortRecurs(left);
+            MergeSortRecurs(right);
+
+            Merge(input, left, right);
+        }
+
+        private void Merge(int[] input, int[] left, int[] right)
+        {
+            int i = 0;
+            int j = 0;
+            while (i < left.Length || j < right.Length)
+            {
+                if (i == left.Length)
+                {
+                    input[i+j] = right[j++];
+                }
+                else if (j == right.Length)
+                {
+                    input[i+j] = left[i++];
+                }
+                else if (left[i] <= right[j])
+                {
+                    input[i+j] = left[i++];
+                }
+                else if (right[j] < left[i])
+                {
+                    input[i+j] = right[j++];
+                }
+            }
+        }
+
+        private long MergeSort_CountInvRecurs(long[] input)
+        {
+            if (input.Length < 2) return 0;
+
+            long mid = (input.Length + 1) / 2;
+            long[] left = new long[mid];
+            long[] right = new long[input.Length - mid];
+
+            Array.Copy(input, 0, left, 0, mid);
+            Array.Copy(input, mid, right, 0, input.Length - mid);
+
+            return MergeSort_CountInvRecurs(left) + MergeSort_CountInvRecurs(right) + Merge_CountInv(input, left, right);
+        }
+
+        private long Merge_CountInv(long[] input, long[] left, long[] right)
+        {
+            long i = 0;
+            long j = 0;
+            long count = 0;
+            while (i < left.Length || j < right.Length)
+            {
+                if (i == left.Length)
+                {
+                    input[i + j] = right[j++];
+                }
+                else if (j == right.Length)
+                {
+                    input[i + j] = left[i++];
+                }
+                else if (left[i] <= right[j])
+                {
+                    input[i + j] = left[i++];
+                }
+                else if (right[j] < left[i])
+                {
+                    input[i + j] = right[j++];
+                    count += left.Length - i;
+                }
+            }
+            return count;
+        }
+
+    }
+}
---
+++ DivideAndConquerTests/MergeTest.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using DivideAndConquer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ploeh.AutoFixture;
+
+namespace DivideAndConquerTests
+{
+    [TestClass]
+    public class DivideAndConquerTest
+    {
+        private readonly SortLib _lib = new SortLib();
+
+        [TestMethod]
+        public void TestMergeSort()
+        {
+            var input = new[] {2, 4, 5, 1, 7, 8, 3, 6};
+            var result = _lib.MergeSort(input);
+            CollectionAssert.AreEqual(result, new [] {1, 2, 3, 4, 5, 6, 7, 8});
+        }
+
+        [TestMethod]
+        public void TestQuickSort()
+        {
+            var input = new[] { 2, 4, 5, 1, 7, 8, 3, 6 };
+            var rand = new Random(DateTime.Now.Millisecond);
+            var nbComparisons = _lib.QuickSort(input, (ints, l, r) => rand.Next(l, r));
+            CollectionAssert.AreEqual(input, new[] { 1, 2, 3, 4, 5, 6, 7, 8 });
+        }
+
+        [TestMethod]
+        public void TestQuickSortCountComparisons()
+        {
+            var input = new[] { 2, 4, 5, 1, 7, 8, 3, 6 };
+            Func<int[], int, int, int> choosePivot = (ints, left, right) => left;
+            var nbComparisons = _lib.QuickSort(input, choosePivot);
+            CollectionAssert.AreEqual(input, new[] { 1, 2, 3, 4, 5, 6, 7, 8 });
+            Assert.IsTrue(nbComparisons == 16);
+        }
+
+        [TestMethod]
+        public void TestQuickSortCountComparisonsWithMedianPivot()
+        {
+            var inputTab = new[] { 2, 4, 5, 1, 7, 8, 3, 6 };
+            Func<int[], int, int, int> choosePivot = (input, left, right) =>
+            {
+                var length = right - left + 1;
+                var l = input[left];
+                var r = input[right];
+                var medIndex = length % 2 == 0 ? (length / 2) - 1 : length / 2;
+                var m = input[medIndex];
+                var tab = new int[] { l, r, m };
+                tab = tab.OrderBy(t => t).ToArray();
+                return tab[1] == l
+                    ? left
+                    : tab[1] == r
+                        ? right
+                        : medIndex;
+            };
+            var nbComparisons = _lib.QuickSort(inputTab, choosePivot);
+            CollectionAssert.AreEqual(inputTab, new[] { 1, 2, 3, 4, 5, 6, 7, 8 });
+            Assert.IsTrue(nbComparisons == 16);
+        }
+
+        [TestMethod]
+        public void TestQuickSortCountComparisonsFromFileWithPivotFirstElement()
+        {
+            var filePath = @"c:\QuickSort.txt";
+            Func<int[], int, int, int> choosePivot = (input, left, right) => left;
+            var nbComparisons = _lib.CountComparisonInQuickSortFromFile(filePath, choosePivot);
+            Assert.IsTrue(nbComparisons == 162085);
+        }
+
+        [TestMethod]
+        public void TestQuickSortCountComparisonsFromFileWithPivotLastElement()
+        {
+            var filePath = @"c:\QuickSort.txt";
+            Func<int[], int, int, int> choosePivot = (input, left, right) => right;
+            var nbComparisons = _lib.CountComparisonInQuickSortFromFile(filePath, choosePivot);
+            Assert.IsTrue(nbComparisons == 164123);
+        }
+
+        [TestMethod]
+        public void TestQuickSortCountComparisonsFromFileWithPivotMedianElement()
+        {
+            var filePath = @"c:\QuickSort.txt";
+            Func<int[], int, int, int> choosePivot = (input, left, right) =>
+            {
+                var length = right - left + 1;
+                var l = input[left];
+                var r = input[right];
+                var medIndex = length % 2 == 0 ? (length / 2) - 1 : length / 2;
+                var m = input[medIndex + left];
+                var tab = new[] { l, r, m };
+                tab = tab.OrderBy(t => t).ToArray();
+                return tab[1] == l
+                    ? left
+                    : tab[1] == r
+                        ? right
+                        : medIndex + left;
+            };
+            var nbComparisons = _lib.CountComparisonInQuickSortFromFile(filePath, choosePivot);
+            Assert.IsTrue(nbComparisons == 138382);
+        }
+
+        [TestMethod]
+        public void TestFindMedian()
+        {
+            Func<int[], int, int, int> choosePivot = (input, left, right) =>
+            {
+                var length = right - left + 1;
+                var l = input[left];
+                var r = input[right];
+                var medIndex = length % 2 == 0 ? (length / 2) - 1 : length / 2;
+                var m = input[medIndex + left];
+                var tab = new[] { l, r, m };
+                tab = tab.OrderBy(t => t).ToArray();
+                return tab[1] == l
+                    ? left
+                    : tab[1] == r
+                        ? right
+                        : medIndex + left;
+            };
+            var inputA = new[] { 8, 2, 4, 5, 7, 1 };
+            var inputB = new[] { 4, 2, 8, 5, 7, 1 };
+            var inputE = new[] { 1, 2, 8, 5, 7, 4 };
+            var inputC = new[] { 8, 2, 4, 5, 7, 1, 3 };
+            var inputD = new[] { 1, 2, 4, 3, 7, 1, 2 };
+            var inputF = new[] { 2, 2, 4, 1, 7, 1, 3 };
+            var inputI = new[] { 3, 2, 4, 1};
+            Assert.IsTrue(choosePivot(inputA, 0, inputA.Length - 1) == 2);
+            Assert.IsTrue(choosePivot(inputB, 0, inputB.Length - 1) == 0);
+            Assert.IsTrue(choosePivot(inputE, 0, inputE.Length - 1) == 5);
+            Assert.IsTrue(choosePivot(inputE, 2, inputE.Length - 1) == 3);
+            Assert.IsTrue(choosePivot(inputC, 0, inputC.Length - 1) == 3);
+            Assert.IsTrue(choosePivot(inputD, 0, inputD.Length - 1) == 6);
+            Assert.IsTrue(choosePivot(inputF, 0, inputF.Length - 1) == 0);
+            Assert.IsTrue(choosePivot(inputI, 0, inputI.Length - 1) == 1);
+        }
+
+        [TestMethod]
+        public void TestQuickSortBis()
+        {
+            var rand = new Random(DateTime.Now.Millisecond);
+            var fixture = new Fixture();
+            var input = fixture.CreateMany<int>(1000).ToArray();
+            var watch = new Stopwatch();
+            watch.Start();
+            var nbComparisons = _lib.QuickSort(input, (ints, l, r) => rand.Next(l, r));
+            watch.Stop();
+            var quickSortTime = watch.ElapsedMilliseconds;
+            CollectionAssert.AreEqual(input, input.OrderBy(i => i).ToArray());
+        }
+
+        [TestMethod]
+        public void TestMergeSortBis()
+        {
+            var input = new[] { 2, 4, 5, 9, 1, 7, 8, 3, 6 };
+            var result = _lib.MergeSort(input);
+            CollectionAssert.AreEqual(result, new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+        }
+
+        [TestMethod]
+        public void TestCountInvInFile()
+        {
+            var filePath = @"c:\IntegerArray.txt";
+            var result = _lib.CountInvInFile(filePath);
+            Assert.IsTrue(result > 0);
+        }
+
+        [TestMethod]
+        public void TestInvCount()
+        {
+            var input = new long[] { 2, 4, 5, 1, 7, 8, 3, 6 };
+            var result = _lib.CountInv(input);
+            Assert.AreEqual(result, 9);
+        }
+
+        [TestMethod]
+        public void TestInvCountBis()
+        {
+            var input = new long[] { 3, 2, 1, 4, 6, 5, 7 };
+            var result = _lib.CountInv(input);
+            Assert.AreEqual(result, 4);
+        }
+
+        [TestMethod]
+        public void TestBubbleSort()
+        {
+            var input = new[] { 2, 4, 5, 1, 7, 8, 3, 6 };
+            var result = _lib.BubbleSort(input);
+            CollectionAssert.AreEqual(result, new[] { 1, 2, 3, 4, 5, 6, 7, 8 });
+        }
+
+        [TestMethod]
+        public void TestLargeSort()
+        {
+            var fixture = new Fixture();
+            var input_1 = fixture.CreateMany<int>(1000).ToArray();
+            var input_2 = fixture.CreateMany<int>(1000).ToArray();
+            var input_3 = fixture.CreateMany<int>(1000).ToArray();
+            var watch = new Stopwatch();
+            watch.Start();
+            var result = _lib.MergeSort(input_1);
+            watch.Stop();
+            var mergeSortTime = watch.ElapsedMilliseconds;
+            CollectionAssert.AreEqual(result, input_1.OrderBy(i => i).ToArray());
+            watch.Reset();
+            
+            var rand = new Random(DateTime.Now.Millisecond);
+            watch.Start();
+            var nbComparisons = _lib.QuickSort(input_2, (ints, l, r) => rand.Next(l, r));
+            watch.Stop();
+            var qsResult = new int[1000];
+            Array.Copy(input_2, qsResult, 1000);
+            var quickSortTime = watch.ElapsedMilliseconds;
+            CollectionAssert.AreEqual(qsResult, input_2.OrderBy(i => i).ToArray());
+            watch.Reset();
+
+            watch.Start();
+            result = _lib.BubbleSort(input_3);
+            watch.Stop();
+            var bubbleSortTime = watch.ElapsedMilliseconds;
+            CollectionAssert.AreEqual(result, input_3.OrderBy(i => i).ToArray());
+            Assert.IsTrue(bubbleSortTime > mergeSortTime && bubbleSortTime > quickSortTime);
+
+            watch.Start();
+        }
+
+        [TestMethod]
+        public void CompareMergesortQuicksort()
+        {
+            var fixture = new Fixture();
+            var input_1 = fixture.CreateMany<int>(100000).ToArray();
+            var input_2 = fixture.CreateMany<int>(100000).ToArray();
+            var watch = new Stopwatch();
+            watch.Start();
+            var result = _lib.MergeSort(input_1);
+            watch.Stop();
+            var mergeSortTime = watch.ElapsedMilliseconds;
+            CollectionAssert.AreEqual(result, input_1.OrderBy(i => i).ToArray());
+            watch.Reset();
+
+            var rand = new Random(DateTime.Now.Millisecond);
+            watch.Start();
+            var nbComparisons = _lib.QuickSort(input_2, (ints, l, r) =>  rand.Next(l, r));
+            watch.Stop();
+            var qsResult = new int[100000];
+            Array.Copy(input_2, qsResult, 100000);
+            var quickSortTime = watch.ElapsedMilliseconds;
+            CollectionAssert.AreEqual(qsResult, input_2.OrderBy(i => i).ToArray());
+            watch.Reset();
+        }
+    }
+}
